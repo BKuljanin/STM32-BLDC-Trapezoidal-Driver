d31eb24e760287331639b331a3d40371cc5152f0
BogdanKuljanin <kuljaninbogdan@gmail.com>
2026-05-10 00:33:56 +0200

    using injected ADC removing standard queue. replacing dma with interrupt

--- Core/Inc/adc.h
+++ Core/Inc/adc.h
@@ -1,9 +1,14 @@
 #ifndef INC_ADC_H_
 #define INC_ADC_H_
 
-void back_emf_adc_dma_init(uint32_t src, uint32_t dst);
+#define VREF 3.3f
+#define ADC_RES 4095.0f
+#define DIVIDER_RATIO 7.66f
+
+void back_emf_adc_dma_init(void);
 void tim2_trgo_adc(void);
 float adc_to_volts(uint32_t adc_value);
 float adc_to_amps(float voltage);
+
 
 #endif /* INC_ADC_H_ */
